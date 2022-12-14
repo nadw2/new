73366dfd73a7e7c070e9efaa0a4412d556c76a06
Надежда Трактирникова <nadezdatraktirnikova@MacBook-Air-Nadezda.local>
2022-12-14 17:31:57 +0700

    first

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Ate you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?','18');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers [0] = prompt('Как ваше имя?','');
+
+// answers [1] = prompt('Как ваше фамилия?','');
+
+// answers [2] = prompt('Сколько вам лет ?','');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет,${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(incr++);
+console.log(decr--);
+
+const isChecked = true,
+        isClose = false;
+
+        console.log(isChecked || isClose );
